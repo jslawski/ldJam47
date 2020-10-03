24ab61875179ee71061f9d0a16db3ce1ecab6fbf
Jared <jslawski@umich.edu>
2020-10-03 02:28:07 -0600

    Character movement and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public static CameraFollow instance;
+
+    public HorseController playerCharacter;
+
+    private Camera thisCamera;
+    private Transform cameraTransform;
+    private float cameraDistance;
+
+    private float verticalViewportThreshold = 0.3f;
+    private float horizontalViewportThreshold = 0.4f;
+
+    void Awake()
+    {
+        CameraFollow.instance = this;
+        this.thisCamera = this.gameObject.GetComponentInChildren<Camera>();
+        this.cameraTransform = this.gameObject.transform;
+        this.cameraDistance = this.cameraTransform.position.y;
+    }
+
+    private bool IsPlayerPastHorizontalThreshold(float playerViewportXPosition)
+    {
+        return (playerViewportXPosition > (1.0f - this.horizontalViewportThreshold)) ||
+            (playerViewportXPosition < (0.0f + this.horizontalViewportThreshold));
+    }
+
+    private bool IsPlayerPastVerticalThreshold(float playerViewportYPosition)
+    {
+        return (playerViewportYPosition > (1.0f - this.verticalViewportThreshold)) ||
+            (playerViewportYPosition < (0.0f + this.verticalViewportThreshold));
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 playerViewportPosition = thisCamera.WorldToViewportPoint(this.playerCharacter.gameObject.transform.position);
+
+        if (this.playerCharacter != null && this.IsPlayerPastVerticalThreshold(playerViewportPosition.y))
+        {
+            this.UpdateCameraVerticalPosition();
+        }
+
+        if (this.IsPlayerPastHorizontalThreshold(playerViewportPosition.x))
+        {
+            this.UpdateCameraHorizontalPosition();
+        }
+    }
+
+    private void UpdateCameraVerticalPosition()
+    {
+        Vector3 worldSpaceCenteredPosition = this.thisCamera.ViewportToWorldPoint(new Vector3(0.5f, this.cameraDistance, this.verticalViewportThreshold));
+
+        Vector3 shiftVector = new Vector3(0, 0, this.playerCharacter.transform.position.z - worldSpaceCenteredPosition.z);
+
+        this.cameraTransform.Translate(shiftVector.normalized * Mathf.Abs(this.playerCharacter.Body.velocity.z) * Time.deltaTime);
+    }
+
+    private void UpdateCameraHorizontalPosition()
+    {
+        Vector3 worldSpaceCenteredPosition = this.thisCamera.ViewportToWorldPoint(new Vector3(0.5f, this.cameraDistance, this.verticalViewportThreshold));
+
+        Vector3 shiftVector = new Vector3(this.playerCharacter.transform.position.x - worldSpaceCenteredPosition.x, 0, 0);
+
+        this.cameraTransform.Translate(shiftVector.normalized * Mathf.Abs(this.playerCharacter.Body.velocity.x) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/HorseController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class HorseController : MonoBehaviour
+{
+    [SerializeField, Range(0, 100)]
+    private float maxSpeed = 10f;
+    [SerializeField, Range(0, 100)]
+    private float maxAcceleration = 30f;
+    [SerializeField, Range(0, 100)]
+    private float maxDeceleration = 10f;
+
+    private Vector3 playerVelocity;
+    private Vector3 desiredPlayerVelocity;
+    private float desiredMaxSpeedChange;
+
+    public Rigidbody Body { get; private set; }
+
+    [SerializeField]
+    private KeyCode upKey;
+    [SerializeField]
+    private KeyCode downKey;
+    [SerializeField]
+    private KeyCode leftKey;
+    [SerializeField]
+    private KeyCode rightKey;
+
+    private void PauseVelocityMovement()
+    {
+        this.Body.velocity = Vector3.zero;
+        this.Body.angularVelocity = Vector3.zero;
+    }
+
+    private void Start()
+    {
+        this.Body = GetComponent<Rigidbody>();
+    }
+
+    private void AccelerateTowardsDesiredVelocity(Vector3 desiredVelocity, float maxSpeedChange)
+    {
+        this.playerVelocity.x = Mathf.MoveTowards(this.playerVelocity.x, desiredVelocity.x, maxSpeedChange);
+        this.playerVelocity.z = Mathf.MoveTowards(this.playerVelocity.z, desiredVelocity.z, maxSpeedChange);
+    }
+
+    private void Update()
+    {
+        this.playerVelocity = this.Body.velocity;
+        Vector3 compositeDirectionVector = this.GetCompositeDirectionVector();
+
+        this.desiredPlayerVelocity = compositeDirectionVector.normalized * this.maxSpeed;
+        this.desiredMaxSpeedChange = this.maxAcceleration;
+    }
+
+    private Vector3 GetCompositeDirectionVector()
+    {
+        Vector3 compositeVector = new Vector3();
+
+        if (Input.GetKey(this.upKey))
+        {
+            compositeVector += Vector3.forward;
+        }
+        if (Input.GetKey(this.downKey))
+        {
+            compositeVector += Vector3.back;
+        }
+        if (Input.GetKey(this.leftKey))
+        {
+            compositeVector += Vector3.left;
+        }
+        if (Input.GetKey(this.rightKey))
+        {
+            compositeVector += Vector3.right;
+        }
+
+        return compositeVector;
+    }
+
+    private void FixedUpdate()
+    {
+        float maxSpeedChange = this.desiredMaxSpeedChange * Time.deltaTime;
+
+        this.AccelerateTowardsDesiredVelocity(this.desiredPlayerVelocity, maxSpeedChange);
+        if (this.playerVelocity.magnitude < 0)
+        {
+            this.playerVelocity = Vector3.zero;
+        }
+
+        this.Body.velocity = this.playerVelocity;
+    }
+}
