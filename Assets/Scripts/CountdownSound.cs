096551790e2ad671e68558747a307ef950ba1598
Jared <jslawski@umich.edu>
2020-10-05 00:54:50 -0600

    Countdown sound effects

--- Assets/Scripts/CountdownSound.cs
+++ Assets/Scripts/CountdownSound.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class CountdownSound : MonoBehaviour
 {
+    public AudioSource sizzleSound;
+    public AudioSource countdownSound;
+
     public void PlayStartSound()
     {
         VoiceLineManager.instance.PlayRandomStartLine();
     }
 
+    public void PlaySizzleSound()
+    {
+        this.sizzleSound.Play();
+    }
+
+    public void PlayCountdownSound()
+    {
+        this.countdownSound.Play();
+    }
 
     public void DestroyCountdownObject()
     {
